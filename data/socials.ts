62440bdd8b3b7696c164b01470a5230ce46ad2be
hasithaea <hasitha2k4@gmail.com>
2026-09-26 23:36:46 +0530

    refactor: extract socials data (#18)

---
+++ data/socials.ts
@@ -0,0 +1,27 @@
+export type Socials = {
+    name: string;
+    href: string;
+};
+
+export const socials: Socials[] = [
+  {
+    name: "GitHub",
+    href: "https://github.com/hasithaea",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/hasithaea",
+  },
+  {
+    name: "Docker Hub",
+    href: "https://hub.docker.com/u/hasithaea",
+  },
+  {
+    name: "X",
+    href: "https://x.com/hasithaea",
+  },
+  {
+    name: "Discord",
+    href: "https://discord.com/users/hasithaea",
+  },
+];
